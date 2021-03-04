025a80fa5878f84e5e6e6b5df84c419bba3f2bb7
revuen <romansu@outlook.com>
2021-03-05 00:20:40 +0200

    guard the clicks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { BoardService } from './board.service';
 
 @Component({
@@ -9,10 +9,51 @@ import { BoardService } from './board.service';
 export class AppComponent implements OnInit {
   board = [];
   boardSize = 4;
-  emptyCord = [this.boardSize - 1, this.boardSize - 1];
+  clickLog = [];
+  private emptyCell = [this.boardSize - 1, this.boardSize - 1];
+  private ROW = 0;
+  private COL = 1;
+
   constructor(private readonly boardService: BoardService) {}
 
-  public ngOnInit(): void {
+  ngOnInit(): void {
     this.board = this.boardService.initBoard(this.boardSize);
   }
+
+  public move(x: number, y: number): void {
+    if (this.validMove({ x, y })) {
+      this.logMoves({ x, y });
+      const tempCell = this.board[x][y];
+      this.board[x][y] = null;
+      this.board[this.emptyCell[this.ROW]][this.emptyCell[this.COL]] = tempCell;
+      this.emptyCell = [x, y];
+    }
+  }
+
+  private logMoves({ x, y }: ICoordinates): void {
+    this.clickLog.push({
+      from: { x, y },
+      to: {
+        x: this.emptyCell[this.ROW],
+        y: this.emptyCell[this.COL],
+      },
+    });
+  }
+
+  private validMove({ x, y }: ICoordinates): boolean {
+    const isNeighbor =
+      Math.abs(this.emptyCell[this.ROW] - x) <= 1 &&
+      Math.abs(this.emptyCell[this.COL] - y) <= 1;
+
+    const isNotDiagonalNeighbor =
+      Math.abs(this.emptyCell[this.ROW] - x) !==
+      Math.abs(this.emptyCell[this.COL] - y);
+
+    return isNeighbor && isNotDiagonalNeighbor;
+  }
+}
+
+interface ICoordinates {
+  x: number;
+  y: number;
 }
